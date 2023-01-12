d80609b6dca67af3d48f1401b3800c0bc3e50414
Maaz Khan <85396969+maazkhansw5145@users.noreply.github.com>
2023-01-12 20:53:06 +0500

    bug fix

--- frontend/src/Config/URL.js
+++ frontend/src/Config/URL.js
@@ -1,4 +1,4 @@
-// const url = 'http://localhost:4001';
-const url = "https://git.heroku.com/buyerserver.git";
+const url = 'http://localhost:4001';
+// const url = "https://git.heroku.com/buyerserver.git";
 
 export default url;
